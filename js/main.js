51a43314da604e5f737daa1bb7671f13ac7682ee
aylok1n <nikolya.blog@gmail.com>
2021-05-01 20:12:43 +0400

    погнали

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 let scoreCounter = document.getElementById('score');
 let login;
-// const arr = [];
 const numbers = [];
 let span = document.querySelectorAll('span');
 let spanValues = [];
@@ -12,8 +11,8 @@ let score = 0;
 
 function start() {
   login = document.getElementById("login").value
-  document.getElementById("str0").hidden = false;
-  document.getElementById("str1").hidden = true;
+  document.getElementById("HUDBlock").hidden = false;
+  document.getElementById("StartScreen").hidden = true;
   document.getElementById("str2").hidden = false;
   document.getElementById('player').innerHTML = "Игрок: " + login;
   window.setInterval(timer, 1000);
@@ -22,7 +21,7 @@ function start() {
 
   alert("Привет " + login + " Тебе дан список чисел, для победы тебе нужно переставить их в порядке возрастания. Для перестановки кликни на 2 нужных числа. Желаю Удачи! ");
 
-  spawnArr();
+
   
   let arr = spanValues.slice(0);
   bestResult = arr.sort(function (a, b) {
@@ -44,60 +43,35 @@ function timer() {
   elem.value = parseInt(elem.value) + 1; //parseInt преобразует строку в число
 }
 
-function spawnArr() {
-  span.forEach(function (e) {
-    e.textContent = Math.round(Math.random() * 1000);
-    if (e.textContent < 100) {
-      e.textContent = Math.round(Math.random() * 1000);
-    }
-    spanValues.push(e.textContent);
-  })
-}
 
-function showArr(arr) {
-  for (let i = 0; i < 10; i++) {
-    document.getElementById(i).innerHTML = arr[i];
-  }
-}
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-function findPrevious(elm) {
-  numbers.push(elm)
-}
+// ПОБЕДНЫЙ ЭКРАН БРАТЬ ОТСЕДА
 
-function swap(elm) {
-  var previous = findPrevious(elm);
-  if (numbers.length > 1) {
+// function replace(element1, element2) {
+//   var clonedElement1 = element1.cloneNode(true);
+//   var clonedElement2 = element2.cloneNode(true);
+//   element1.textContent = clonedElement2.textContent;
+//   element2.textContent = clonedElement1.textContent;
+//   while (numbers.length) {
+//     numbers.pop()
+//   }
+//   score++;
+//   scoreCounter.innerHTML = "Счет: " + score;
 
-    var clonedElement1 = numbers[0];
-    var clonedElement2 = numbers[1];
-    replace(clonedElement1, clonedElement2)
-  }
-}
+//   currentArr = [];
+//   span.forEach(function(e) {
+//     currentArr.push(e.textContent)
+//   });
 
-function replace(element1, element2) {
-  var clonedElement1 = element1.cloneNode(true);
-  var clonedElement2 = element2.cloneNode(true);
-  element1.textContent = clonedElement2.textContent;
-  element2.textContent = clonedElement1.textContent;
-  while (numbers.length) {
-    numbers.pop()
-  }
-  score++;
-  scoreCounter.innerHTML = "Счет: " + score;
-
-  currentArr = [];
-  span.forEach(function(e) {
-    currentArr.push(e.textContent)
-  });
-
-  if (currentArr.toString() == bestResult.toString()) {
-    let timeToWin = document.getElementById('timer').textContent
-    let points = 100 - (score * 5 - (timeToWin / 5));
-    alert(`Победа! Ваше время: ${timeToWin}. Вы сделали ${score} ходов и набрали ${points} очков. Обновите страницу для новой игры.`)
+//   if (currentArr.toString() == bestResult.toString()) {
+//     let timeToWin = document.getElementById('timer').textContent
+//     let points = 100 - (score * 5 - (timeToWin / 5));
+//     alert(`Победа! Ваше время: ${timeToWin}. Вы сделали ${score} ходов и набрали ${points} очков. Обновите страницу для новой игры.`)
     
-    localStorage.setItem(login, points)
-  }
+//     localStorage.setItem(login, points)
+//   }
 
-  return clonedElement1;
-}
+//   return clonedElement1;
+// }
 
